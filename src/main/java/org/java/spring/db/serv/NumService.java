14c06f7d6f1a7fb01d24e3086cdf4fae6b6926ab
marcobusacca <marcobusacca01@gmail.com>
2024-01-25 16:39:09 +0100

    [FEAT] Service of Model "Num"

---
+++ src/main/java/org/java/spring/db/serv/NumService.java
@@ -0,0 +1,29 @@
+package org.java.spring.db.serv;
+
+import java.util.List;
+
+import org.java.spring.db.pojo.Num;
+import org.java.spring.db.repo.NumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NumService {
+
+	@Autowired
+	private NumRepository numRepository;
+	
+	private List<Num> findAll(){
+		return numRepository.findAll();
+	}
+	private Num findById(int id) {
+		return numRepository.findById(id).get();
+	}
+	public void save(Num num) {
+		numRepository.save(num);
+	}
+	public void delete(Num num) {
+		
+		numRepository.delete(num);
+	}
+}
